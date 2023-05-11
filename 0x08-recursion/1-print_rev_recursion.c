2d6900ea1114584486c19efe69171c4c1ab482c1
Manal Dihane <dihanemanal@gmail.com>
2023-05-10 17:04:56 -0700

    task 0 and 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		_puts_recursion(s + 1);
 	}
-
-	else
-		_putchar('\n');
 }
+
 
